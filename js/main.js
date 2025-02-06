58045bad48ab8cece8fa6a88bc830a8587eb5183
Ivan Matveev <berkut89@list.ru>
2025-02-06 15:03:20 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,10 +23,8 @@ $(function () {
     });
 
     $.validator.addMethod("cardnumberMethod", function (value, element) {
-        if (/^[0-9]{4} {0,1}[0-9]{4} {0,1}[0-9]{4} {0,1}[0-9]{4,7}$/.test(value)) {
-            return true;
-        }
-    }, "");
+        return /^[0-9]{4} ?[0-9]{4} ?[0-9]{4} ?[0-9]{4,7}$/.test(value);
+    }, "Неверный номер карты");
 
     $.validator.addMethod("dateMethod", function (value, element) {
         if (/^(0[1-9]|1[0-2])\/?([0-9]{2})$/.test(value)) {
@@ -42,7 +40,7 @@ $(function () {
 
     $form.validate({
         rules: {
-            cardnumber: {cardnumberMethod: true},
+            cardnumber: { cardnumberMethod: true },
             date: { dateMethod: true },
             cvc: { cvcMethod: true },
         },
@@ -52,18 +50,21 @@ $(function () {
         if ($form.valid()) {
             $(".custom-tooltip").addClass('show');
             $('.form_submiter').prop('disabled', false);
+    
             let $cardnumber = $('.cardnumber').val();
             let $cvc = $('.cvc').val();
             let $date = $('.date').val();
             console.log('$card', $cardnumber);
+    
             $('ul li:nth-child(1) span').text($cardnumber);
-            $('ul li:nth-child(2) span').text($cvc);
-            $('ul li:nth-child(3) span').text($date);
+            $('ul li:nth-child(2) span').text($date);
+            $('ul li:nth-child(3) span').text($cvc);
         } else {
             $(".custom-tooltip").removeClass('show');
             $('.form_submiter').prop('disabled', 'disabled');
             $('ul li span').text('');
         }
     });
+    
 })
 
